f1bcd010f393e83ce8bf8ddd891cb8599c00586b
md.Shahriar Ovi <83135100+SHAHRIAR-SO@users.noreply.github.com>
2022-06-12 16:52:25 +0600

    index.php and manage.php added

---
+++ admin/manage-admin.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/manage-admin.css">
+    <title>Food Order Website-Home Page</title>
+</head>
+
+<body>
+    
+    <!---menu section Starts------>
+    <div class="menu text-center" >
+        <div class="wrapper">
+        <ul>
+            <li> <a href="">Home</a></li>
+            <li> <a href="">Admin</a></li>
+            <li> <a href="">Category</a></li>
+            <li> <a href="">Food</a></li>
+            <li> <a href="">Order</a></li>
+        </ul>
+        </div>
+    </div>
+    <!---menu section Ends------>
+
+
+    <!---main content section start--->
+    <div class="main-content">
+       
+        <div class="wrapper">
+        <h1> <strong>
+        DASHBOARD
+     </strong></h1>
+     <!---box1-->
+     <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Categories
+     </div>
+<!---box2-->
+     <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Categories
+     </div>
+<!---box3-->
+     <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Categories
+     </div>
+<!---box4-->
+<div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Categories
+     </div>
+     <div class="clearfix"></div>
+        </div>
+    </div>
+    <!---main content section ends--->
+
+
+
+    <!---footer section start--->
+    <div class="footer">
+     <div class="wrapper">
+     <p class="text-center">2022 all rights reserved, some restaurent. developed By - <a href="">Shahriar ovi</a> </p>
+     </div>
+    </div>
+    <!---footer section ends--->
+
+</body>
+
+</html>
